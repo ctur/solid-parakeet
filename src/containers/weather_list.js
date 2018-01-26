0139368b71b563a1d37c700d9a6b12cad0e28067
ctur <cturesoy@gmail.com>
2018-01-26 11:40:20 +0300

    Added sparklines module for weather graphs.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine }
 
 class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    
+
+    return (
+        <tr key={name}>
+            <td>{name}</td>
+        </tr>
+    );
+  };
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,6 +28,7 @@ class WeatherList extends Component {
             </tr>
         </thead>
         <tbody>
+            {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
