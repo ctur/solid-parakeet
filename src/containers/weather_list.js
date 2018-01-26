f76928337073b784c3b8ee93564343bfece4d6a2
ctur <cturesoy@gmail.com>
2018-01-26 12:29:38 +0300

    Some style added.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine }
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidites = cityData.list.map(weather => weather.main.humidity);
 
     return (
         <tr key={name}>
             <td>{name}</td>
+
+            <td><Chart data={temps} color="orange" units="C" /></td>
+            <td><Chart data={pressures} color="green" units="hPa"/></td>
+            <td><Chart data={humidites} color="black" units="%"/></td>
         </tr>
+        
     );
   };
 
@@ -22,9 +29,9 @@ class WeatherList extends Component {
         <thead>
             <tr>
                 <th>City</th>
-                <th>Temperature</th>
-                <th>Pressure</th>
-                <th>Humidity</th>
+                <th>Temperature (C)</th>
+                <th>Pressure (hPa)</th>
+                <th>Humidity (%)</th>
             </tr>
         </thead>
         <tbody>
